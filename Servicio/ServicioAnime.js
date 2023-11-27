92c2073ad2052bdca667f53ca3601bb28d18364a
Antonio del Pardo <antonio.delpardo229161@potros.itson.edu.mx>
2023-11-27 14:43:51 -0700

    Update ServicioAnime.js

--- Servicio/ServicioAnime.js
+++ Servicio/ServicioAnime.js
@@ -2,6 +2,19 @@ export class ServicioAnime {
   #urlService = "http://localhost:3000/api/";
   #urlAnimes = this.#urlService + "animes/";
   #urlCapitulos = this.#urlService + "mediaContents/";
+  #urlUsuarios = this.#urlService + "usuarios/";
+
+  async login(usuario) {
+    let response = await fetch(this.#urlUsuarios + "login", {
+      method: "POST",
+      body: JSON.stringify(usuario),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    let json = await response.json();
+    return json;
+  }
 
   async obtenerAnimes() {
     let response = await fetch(this.#urlAnimes, {});

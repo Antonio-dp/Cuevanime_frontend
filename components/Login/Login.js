a41ba62cfd840fb987fad4085adeb428c23bb42b
MarcoIrineo <marco.irineo228880@potros.itson.edu.mx>
2023-11-27 22:49:48 -0700

    Login funcional front

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,21 +1,64 @@
+import { ServicioAnime } from "../../Servicio/ServicioAnime";
+
 export class Login extends HTMLElement {
-    constructor() {
-      super();
-      //const shadow = this.attachShadow({ mode: 'open' })
-      //shadow.appendChild(template.content.cloneNode(true))
-    }
-    async #render(shadow) {
-      fetch("./assets/Login.html")
-        .then((response) => response.text())
-        .then((html) => {
-          shadow.innerHTML += html;
-        })
-        .catch((error) => console.error("error loading HTML: " + error));
-    }
-    connectedCallback() {
-      const shadow = this.attachShadow({ mode: "open" });
-      this.#render(shadow);
+  #servicioAnime = new ServicioAnime();
+
+  constructor() {
+    super();
+    //const shadow = this.attachShadow({ mode: 'open' })
+    //shadow.appendChild(template.content.cloneNode(true))
+  }
+  async #render(shadow) {
+    fetch("./../assets/Login.html")
+      .then((response) => response.text())
+      .then((html) => {
+        shadow.innerHTML += html;
+        this.#addLoginBehavior(shadow)
+      })
+      .catch((error) => console.error("error loading HTML: " + error));
+  }
+
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: "open" });
+    
+    this.#render(shadow);
+    
+  }
+
+  #addLoginBehavior(shadow) {
+    const button = shadow.getElementById('formLogin')
+
+    button.addEventListener("submit", async (e) => {
+      e.preventDefault();
+      const login = await this.#login(shadow)
+      console.log(login)
+
+      if(!login.message){
+        window.location.href = '/'
+      }else{
+        console.log(login.message)
+      }
+    })
+  }
+
+  async #login(shadow) {
+    console.log("sadadadadas")
+    const nickname = shadow.getElementById('nickname').value
+    const password = shadow.getElementById('password').value
+
+    const user = { 'nickname': nickname, 'password': password }
+
+    console.log(user)
+    try {
+      const response = await this.#servicioAnime.login(user)
+
+      return response
+    } catch (e) {
+      console.log("Error al intentar iniciar sesión")
+      return false
     }
   }
-  
-  customElements.define("login-comp", Login);
+}
+
+
+customElements.define("login-comp", Login);

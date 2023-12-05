fabc6d089dc51709fdab63dcc6bcb6fa8f9acce9
Antonio del Pardo <antonio.delpardo229161@potros.itson.edu.mx>
2023-12-04 17:00:51 -0700

    Creación del filtro del catalogo

--- components/Filter/filter.js
+++ components/Filter/filter.js
@@ -14,10 +14,22 @@ export class Filter extends HTMLElement {
   #addDropdownBehavior(shadow) {
     const button = shadow.querySelector("#options-menu");
     const dropdown = shadow.querySelector("#options");
-
+  
     button.addEventListener("click", () => {
       dropdown.classList.toggle("hidden");
     });
+  
+    dropdown.addEventListener("change", () => {
+      const selectedGenre = dropdown.querySelector('input[name="genre"]:checked').value;
+      this.#filterCatalogueByGenres(selectedGenre);
+    });
+  }
+
+  #filterCatalogueByGenres(genero) {
+    const catalogue = document.querySelector('catalogo-comp');
+    if (catalogue) {
+      catalogue.filtrarPorGenero(genero);
+    }
   }
   connectedCallback() {
     const shadow = this.attachShadow({ mode: "open" });

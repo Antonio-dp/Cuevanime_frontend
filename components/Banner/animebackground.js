c7cbbf5dce132e2e65f8f6e9941b2b9840d3a024
Antonio del Pardo <antonio.delpardo229161@potros.itson.edu.mx>
2023-12-05 23:24:40 -0700

    Ajuste de bannercomponent

--- components/Banner/animebackground.js
+++ components/Banner/animebackground.js
@@ -6,7 +6,7 @@ export class AnimeBackground extends HTMLElement {
   }
 
   async #render(shadow) {
-    fetch("./../assets/Banner.html")
+    fetch("./../assets/AnimeBackground.html")
       .then((response) => response.text())
       .then((html) => {
         shadow.innerHTML += html;
@@ -23,8 +23,19 @@ export class AnimeBackground extends HTMLElement {
     bannerImage.src = animeData.imagenes.bannerHorizontal;
 
     bannerImage.style.width = "100%"; // Ajusta el ancho al 100%
-    bannerImage.style.height = "500px"; // Ajusta la altura automáticamente para mantener la proporción
+    bannerImage.style.height = "495px"; // Ajusta la altura automáticamente para mantener la proporción
     bannerImage.style.objectFit = "cover";
+
+    const smallerImage = shadow.querySelector(".smaller-image");
+    smallerImage.src = animeData.imagenes.bannerHorizontal; // Ajusta el ancho al 100%
+    smallerImage.style.height = "500px";
+    smallerImage.style.boxShadow = "0 0 20px 10px rgba(0, 0, 0, 0.5)"; // Ajusta la altura automáticamente para mantener la proporción
+
+    const synopsisComponent = shadow.querySelector(".sinopsis-anime");
+    const sinopsis = document.createElement('p');
+    sinopsis.textContent = animeData.sinopsis;
+    synopsisComponent.appendChild(sinopsis);
+    shadow.appendChild(synopsisComponent);
   }
 
   connectedCallback() {
